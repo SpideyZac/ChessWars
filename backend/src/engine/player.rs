4bfe581c8dcca95e7030b2d97b439811129a6c93
spideyzac <zacmlesser-7@outlook.com>
2025-03-16 00:05:34 -0700

    feat: init engine + player

---
+++ backend/src/engine/player.rs
@@ -0,0 +1,9 @@
+pub struct Player {
+    mana: f64,
+    pawns_bitboard: u64,
+    bishops_bitboard: u64,
+    knights_bitboard: u64,
+    rooks_bitboard: u64,
+    queens_bitboard: u64,
+    kings_bitboard: u64,
+}
